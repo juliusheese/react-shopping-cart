bd48360b79318007fda42e8ac52661c77efcd810
juliusheese <julius.42.shapiro@gmail.com>
2021-01-07 21:12:15 -0600

    alll done, now just refactoring

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ function App() {
 
 	return (
 		<div className="App">
-			<ProductContext.Provider>
+			<ProductContext.Provider value={{ products, addItem }}>
 				<Navigation cart={cart} />
 
 				{/* Routes */}
 				<Route exact path="/">
-					<Products products={products} addItem={addItem} />
+					<Products />
 				</Route>
 
 				<Route path="/cart">
